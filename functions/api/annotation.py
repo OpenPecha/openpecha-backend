2e2bbc9525b22e13b6caeac13e622e5fc3d56e0a
gdetari <gabor@detari.dev>
2025-04-01 15:53:52 +0800

    Support for dev environment, added migration script from prod to dev, annotation endpoint WIP

---
+++ functions/api/annotation.py
@@ -0,0 +1,36 @@
+import logging
+
+from annotation_model import AnnotationModel
+from firebase_config import db
+from flask import Blueprint, jsonify, request
+
+logger = logging.getLogger(__name__)
+
+annotation_bp = Blueprint("annotation", __name__)
+
+
+def get_duplicate_key(document_id: str):
+    doc = db.collection("annotation").where("document_id", "==", document_id).limit(1).get()
+    return doc[0].id if doc else None
+
+
+@annotation_bp.route("/annotation", methods=["POST"])
+def post_annotation():
+    try:
+        annotation_data = request.get_json()
+        if not annotation_data:
+            return jsonify({"error": "Invalid JSON object"}), 400
+
+        annotation = AnnotationModel.model_validate(annotation_data)
+        if duplicate_key := get_duplicate_key(annotation.document_id):
+            return (
+                jsonify({"error": f"Document '{annotation.document_id}' already used to annotate: {duplicate_key}"}),
+                400,
+            )
+
+        db.collection("annotations").document().set(annotation.model_dump())
+        return jsonify({"message": "Annotation created successfully"}), 201
+
+    except Exception as e:
+        logger.error("Error creating annotation: %s", str(e))
+        return jsonify({"error": "Failed to create annotation"}), 500
