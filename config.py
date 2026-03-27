5db97332846d1d6e1fb1b33c40d67d61b9958512
gdetari <gabor@detari.dev>
2026-03-27 22:05:25 +0800

    deploy.yml fixes

--- config.py
+++ config.py
@@ -12,12 +12,9 @@ class Settings(BaseSettings):
     neo4j_uri: str = ""
     neo4j_username: str = ""
     neo4j_password: str = ""
-    neo4j_database: str = ""
 
     aws_s3_bucket: str = ""
     aws_region: str = ""
-    aws_access_key_id: str = ""
-    aws_secret_access_key: str = ""
 
     search_api_url: str = ""
 
