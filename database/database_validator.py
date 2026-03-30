aedf916f0d79cd28fe8394ebabb7e9fad14fe872
gdetari <gabor@detari.dev>
2026-03-30 13:30:24 +0800

    CI fix

--- database/database_validator.py
+++ database/database_validator.py
@@ -1,12 +1,12 @@
 import logging
 from typing import TYPE_CHECKING
 
+from neo4j import AsyncManagedTransaction
+
 from exceptions import DataNotFoundError, DataValidationError, InvalidRequestError
 from models.contribution import ContributionInput
 
 if TYPE_CHECKING:
-    from neo4j import AsyncManagedTransaction
-
     from models.text import TextInput
 
 logger = logging.getLogger(__name__)
